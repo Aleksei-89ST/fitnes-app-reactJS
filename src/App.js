531138428628ea4cc0d7b1ad3915928d96c435ce
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-11-01 23:34:01 +0400

    incomplete structure

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import { Box } from '@mui/material';
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
-import ExerciseDetail from './pages/ExerciseDetail';
-import Home from './pages/Home';
-
+import { Box } from "@mui/material";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
+import "./App.css";
 
 const App = () => (
-  <Box width="400px" sx={{width: {x1: "1488px"}}} m="auto">
-   <Navbar/>
+  <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+    <Navbar />
     <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/exercise/:id' element={<ExerciseDetail/>}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
     </Routes>
-    <Footer/>
+    <Footer />
   </Box>
 );
 
