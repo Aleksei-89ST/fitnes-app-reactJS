ebc67b26b93292f69aa4988441f173b75333c8e5
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-11-06 20:42:41 +0400

    fixed

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -7,13 +7,13 @@ export const exerciseOptions = {
   },
 };
 
-export const youtubeOptions = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-    'X-RapidAPI-Key': 'a27dd109c7msh87026e1acc5b3d9p1e8edcjsn0b6f14e3cf11',
-  },
-};
+// export const youtubeOptions = {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+//     'X-RapidAPI-Key': '68671a8debmsh33c4eedafa8d62cp1dd3bbjsnb277f5c91308',
+//   },
+// };
 
 export const fetchData = async (url, options) => {
   const response = await fetch(url, options);
